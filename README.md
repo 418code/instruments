8a1a5ff9d14b3e4918058ce84451033c7865a855
Maxim Tolstokory <maxim.code@outlook.com>
2021-04-19 19:14:16 -0800

    feat: make possible to add custom media query breakpoints with pre-written classes inside of it

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ makefiles.sh:
 - bash script for BEM nested file structure creation
 - creates css files for block and optionally element(s) or modifier(s) in blocks folder
 - adds imports for the block and element(s) or modifier(s) to pages/index.css
-- has an option to also add predefined media queries to an existing BEM block and specified elements/options
-- Usage: ./makefiles [-m] bemBlockName [__bemElementOrModifierName]
+- has an option to also media queries to new or existing BEM block and specified elements/options
+- Usage: ./makefiles [-m "425 768 1024"] bemBlockName [__bemElementOrModifierName]
--- makefiles.sh
+++ makefiles.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 media_flag=""
+declare -a media_breakpoints_raw
+declare -a media_breakpoints
 
 print_usage() {
-  printf "\nUsage: ./makefiles bemBlockName __bemElementOrModifierName optional_flags: {-m -> adds pre-defined media queries}\n"
+  printf "\nUsage: ./makefiles [-m \"425 768 1024\"] bemBlockName [__bemElementOrModifierName]\n"
 }
 
 add_media_queries() {
-  printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $1
+  #put all breakpoints even from multiple -m flags into a flat array
+  if [[ ! ${media_breakpoints[@]} > 0 ]]; then
+    for point in "${media_breakpoints_raw[@]}"
+    do
+      media_breakpoints+=($point)
+    done
+  fi
+  for point in "${media_breakpoints[@]}"
+  do
+    printf "\n@media (min-width: "$point"px) {\n  .$2 {\n\n  }\n}\n" >> $1
+  done
 }
 
 #scan for flags
-while getopts 'm' flag; do
+while getopts 'm:' flag; do
   case "${flag}" in
-    m) media_flag='true' ;;
+    m) media_flag='true'
+       media_breakpoints_raw+=("$OPTARG");;
     *) print_usage
        exit 1 ;;
   esac
@@ -31,12 +44,12 @@ if [ "$var" == "$1" ]; then
     touch $blockPath/"$1".css
     printf ".$var {\n\n}\n" >> $blockPath/"$1".css
     if [ "$media_flag" == "true" ]; then
-      add_media_queries $blockPath/"$1".css
+      add_media_queries "$blockPath/$1.css" "$var"
     fi
     printf "\n@import url(../blocks/"$1"/"$1".css);\n" >> ./pages/index.css
   #if the block exist, and media flag, appent media breakpoints to file
   elif [ -d $blockPath ] && [ "$media_flag" == "true" ]; then
-    add_media_queries $blockPath/"$1".css
+    add_media_queries "$blockPath/$1.css" "$var"
   fi
 else
   if [ ! -d $blockPath/"$var" ]; then
@@ -44,12 +57,12 @@ else
     touch $blockPath/"$var"/"$1""$var".css
     printf ".$1$var {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
     if [ "$media_flag" == "true" ]; then
-      add_media_queries $blockPath/"$var"/"$1""$var".css
+      add_media_queries "$blockPath/$var/$1$var.css" "$1$var"
     fi
     printf "@import url(../blocks/$1/"$var"/"$1""$var".css);\n" >> ./pages/index.css
   #if an element/modifier exists, and media flag, append media breakpoints to file
   elif [ -d $blockPath/"$var" ] && [ "$media_flag" == "true" ]; then
-    add_media_queries $blockPath/"$var"/"$1""$var".css
+    add_media_queries "$blockPath/$var/$1$var.css" "$1$var"
   fi
 fi
 
