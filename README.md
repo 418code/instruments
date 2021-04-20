6d9143af2fbd1304f6dea080fc645d4d5f028516
Maxim Tolstokory <maxim.code@outlook.com>
2021-04-19 16:14:28 -0800

    fix: implement media query append logic

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ makefiles.sh:
 - bash script for BEM nested file structure creation
 - creates css files for block and optionally element(s) or modifier(s) in blocks folder
 - adds imports for the block and element(s) or modifier(s) to pages/index.css
-- has an option to also add predefined media queries to all new css files inside the BEM block
-- if called on an existing block, will add new elements/modifiers without overwriting existing elements
-- Usage: ./makefiles bemBlockName [__bemElementOrModifierName] [-m]
+- has an option to also add predefined media queries to an existing BEM block and specified elements/options
+- Usage: ./makefiles [-m] bemBlockName [__bemElementOrModifierName]
--- makefiles.sh
+++ makefiles.sh
@@ -30,15 +30,23 @@ if [ "$var" == "$1" ]; then
       printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$1".css
     fi
     printf "\n@import url(../blocks/"$1"/"$1".css);\n" >> ./pages/index.css
+  #if the block exist, and media flag, appent media breakpoints to file
+  elif [ -d $blockPath ] && [ "$media_flag" == "true" ]; then
+    printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$1".css
   fi
 else
-  mkdir -p $blockPath/"$var"
-  touch $blockPath/"$var"/"$1""$var".css
-  printf ".$1$var {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
-  if [ "$media_flag" == "true" ]; then
+  if [ ! -d $blockPath/"$var" ]; then
+    mkdir -p $blockPath/"$var"
+    touch $blockPath/"$var"/"$1""$var".css
+    printf ".$1$var {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
+    if [ "$media_flag" == "true" ]; then
+      printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
+    fi
+    printf "@import url(../blocks/$1/"$var"/"$1""$var".css);\n" >> ./pages/index.css
+  #if an element/modifier exists, and media flag, append media breakpoints to file
+  elif [ -d $blockPath/"$var" ] && [ "$media_flag" == "true" ]; then
     printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
   fi
-  printf "@import url(../blocks/$1/"$var"/"$1""$var".css);\n" >> ./pages/index.css
 fi
 
 done
