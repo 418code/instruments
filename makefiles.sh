860b4fbc0e50f69a92d2a14f6e587c8c806a5b8c
Maxim Tolstokory <maxim.code@outlook.com>
2021-04-19 17:21:11 -0800

    refactor: make media query addition a function

--- makefiles.sh
+++ makefiles.sh
@@ -6,6 +6,10 @@ print_usage() {
   printf "\nUsage: ./makefiles bemBlockName __bemElementOrModifierName optional_flags: {-m -> adds pre-defined media queries}\n"
 }
 
+add_media_queries() {
+  printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $1
+}
+
 #scan for flags
 while getopts 'm' flag; do
   case "${flag}" in
@@ -27,12 +31,12 @@ if [ "$var" == "$1" ]; then
     touch $blockPath/"$1".css
     printf ".$var {\n\n}\n" >> $blockPath/"$1".css
     if [ "$media_flag" == "true" ]; then
-      printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$1".css
+      add_media_queries $blockPath/"$1".css
     fi
     printf "\n@import url(../blocks/"$1"/"$1".css);\n" >> ./pages/index.css
   #if the block exist, and media flag, appent media breakpoints to file
   elif [ -d $blockPath ] && [ "$media_flag" == "true" ]; then
-    printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$1".css
+    add_media_queries $blockPath/"$1".css
   fi
 else
   if [ ! -d $blockPath/"$var" ]; then
@@ -40,12 +44,12 @@ else
     touch $blockPath/"$var"/"$1""$var".css
     printf ".$1$var {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
     if [ "$media_flag" == "true" ]; then
-      printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
+      add_media_queries $blockPath/"$var"/"$1""$var".css
     fi
     printf "@import url(../blocks/$1/"$var"/"$1""$var".css);\n" >> ./pages/index.css
   #if an element/modifier exists, and media flag, append media breakpoints to file
   elif [ -d $blockPath/"$var" ] && [ "$media_flag" == "true" ]; then
-    printf "\n@media (min-width: 425px) {\n\n}\n\n@media (min-width: 768px) {\n\n}\n\n@media (min-width: 1024px) {\n\n}\n\n@media (min-width: 1280px) {\n\n}\n" >> $blockPath/"$var"/"$1""$var".css
+    add_media_queries $blockPath/"$var"/"$1""$var".css
   fi
 fi
 
