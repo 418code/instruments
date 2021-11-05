4af925dc2e3e5445b056a60d276c69b5693fdaeb
Maxim Tolstokory <maxim.code@outlook.com>
2021-11-05 12:32:28 -0800

    fix: adjust node mongo tools script

--- node_mongo_tools.sh
+++ node_mongo_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gnome-terminal --tab  --working-directory="dev/yandex/express-mesto" -- code .
+gnome-terminal --tab -- code ~/dev/yandex/express-mesto
 gnome-terminal --tab -- postman
 gnome-terminal --tab -- mongodb-compass
 cd dev/yandex/express-mesto && git branch && git status
